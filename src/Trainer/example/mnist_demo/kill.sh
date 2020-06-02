fc34b3f61ae3de8377b7e1088fba3c20ef8d21fe
huangwei19 <huangwei19@jd.com>
2020-06-02 12:22:12 +0800

    add make mnist data

--- src/Trainer/example/mnist_demo/kill.sh
+++ src/Trainer/example/mnist_demo/kill.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-kill -9 `ps -aux | grep python | grep leader | awk '{print $2}'`
-kill -9 `ps -aux | grep python | grep follower | awk '{print $2}'`
+kill -9 `ps -aux | grep python | grep mnist_leader | awk '{print $2}'`
+kill -9 `ps -aux | grep python | grep mnist_follower | awk '{print $2}'`
 kill -9 `ps -aux | grep python | grep dc_leader | awk '{print $2}'`
 kill -9 `ps -aux | grep python | grep dc_follower | awk '{print $2}'`
 kill -9 `ps -aux | grep python | grep co_proxy_server | awk '{print $2}'`
-kill -9 `ps -aux | grep python | grep peer_debug | awk '{print $2}'`
 
 
